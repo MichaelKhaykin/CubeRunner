c1715c6cf3e218e2e9724c8f725f99cc612770f3
MishaTheMiner <mr.khaykin@gmail.com>
2018-10-21 18:16:31 -0700

    Init commit on font effects, some data found was that if you shrink down a font (using scale) it looks good, but if you scale up then it becomes pixelated

---
+++ FontEffectsLibrary/FontEffectsLibrary/TextLabel.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FontEffectsLibrary
+{
+    public class TextLabel
+    {
+        private SpriteFont Font;
+        public Vector2 Position { get; set; }
+
+        private Queue<Color> colorsToLerpThrough;
+        private Color colorToLerpFrom;
+
+        public Color Color { get; set; }
+
+        public float Rotation { get; set; }
+
+        private float travelPercentage;
+
+        private float rateOfChange;
+
+        private string text;
+
+        public Vector2 Origin
+        {
+            get
+            {
+                return new Vector2(Font.MeasureString(text).X / 2, Font.MeasureString(text).Y / 2);
+            }
+            set
+            {
+                Origin = value;
+            }
+        }
+
+        public float Scale { get; set; }
+        
+        public TextLabel(SpriteFont font, Vector2 position, List<Color> colors, float rateOfChange, string text, float scale)
+        {
+            Init(font, position, colors, text, scale);
+            this.rateOfChange = rateOfChange;
+        }
+
+        public TextLabel(SpriteFont font, Vector2 position, Color color, string text, float scale)
+        {
+            Init(font, position, new List<Color>() { color }, text, scale);
+        }
+
+        private void Init(SpriteFont font, Vector2 position, List<Color> color, string text, float scale)
+        {
+            Font = font;
+            Position = position;
+            this.text = text;
+            colorsToLerpThrough = new Queue<Color>(color);
+            Scale = scale;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            Color = Color.Lerp(colorToLerpFrom, colorsToLerpThrough.Peek(), travelPercentage);
+            travelPercentage += rateOfChange;
+
+            if(travelPercentage >= 1)
+            {
+                travelPercentage = 0f;
+
+                colorsToLerpThrough.Enqueue(colorToLerpFrom);
+                colorToLerpFrom = colorsToLerpThrough.Dequeue();
+            }
+        }
+
+        public virtual void Draw(SpriteBatch sb)
+        {
+            sb.DrawString(Font, text, Position, Color, Rotation, Origin, Scale, SpriteEffects.None, 0f);
+        }
+    }
+}
---
+++ FontEffectsLibrary/Tester/Game1.cs
@@ -0,0 +1,79 @@
+﻿using FontEffectsLibrary;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Tester
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        TextLabel textLabel;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            graphics.PreferredBackBufferHeight = 800;
+            graphics.PreferredBackBufferWidth = 1600;
+            graphics.ApplyChanges();
+
+            IsMouseVisible = true;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            List<Color> colors = new List<Color>();
+            colors.Add(Color.Red);
+            colors.Add(Color.Yellow);
+            colors.Add(Color.Green);
+            colors.Add(Color.Blue);
+            colors.Add(Color.Purple);
+
+            textLabel = new TextLabel(Content.Load<SpriteFont>("Font"), new Vector2(800, 400), colors, 0.05f, "Hello", 0.5f);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            textLabel.Update(gameTime);
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+
+            textLabel.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
