e7bbc94448f762f51a33a178e65525bd6a13445f
GMRDL3 <gmrstudent@outlook.com>
2018-11-11 18:00:52 -0800

    Started planning out classes

---
+++ PhysicsLibrary/PhysicsLibrary/PhysicsObject.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysicsLibrary
+{
+    public interface IGameObject
+    {
+        Vector2 Position { get; }
+    }
+
+    public class PhysicsObject
+    {
+        public Rectangle Hitbox;
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
